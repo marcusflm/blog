96c9d9d939be79d13cbfa0922bcc39c4a60696ad
Marcus Mendes <marcus.mendes@tjdft.jus.br>
2024-07-29 16:13:52 +0000

    debug files

--- obj/Debug/net8.0/blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ef5ec004f4e4ce370ad6ac44cccf937cb0fab6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ddada262091abbc0cd1f676b09d1a18398647d")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
