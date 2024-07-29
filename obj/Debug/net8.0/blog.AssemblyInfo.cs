dc8ff86319b3f3aea556229de22431216137a42b
Marcus Mendes <marcusflm@gmail.com>
2024-07-29 18:55:07 +0000

    clean project gitignore

--- obj/Debug/net8.0/blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd31b3ae05c896a30f15a9333daa5007f3b2c19d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd88d54a32532dce9cdf1cf67af36ed38726934")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
